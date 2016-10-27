eb73763bd2e276ba585262c0910f5bd65877b016
Cola Kang <colakang@gmail.com>
2016-10-27 19:59:33 +0000

    Add Email function

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -300,6 +300,12 @@ class Index extends Controller
 						{
 							$data['caseid'] = $case;
 							$data['addcase'] = 'success';
+			 				$sellers = controller('Seller','event');
+							$seller = $sellers->findUserById($data['sellerid']);
+							$text = "New Case In!! Client: ".$data['clientname']." Date:";
+							$sendmail = $cases->sendmail($seller['email'],$data['sellerName'],$text);
+							if ($sendmail)
+								$data['sendmail'] = 'success';
 						} else {
 							$data['addcase'] = 'Fail';
 							$data['reasons'] = 'Data Save Error!!';
@@ -392,7 +398,51 @@ class Index extends Controller
 	public function testmail()
 	{
 		$mail = new \PHPMailer(true);
-		dump($mail);
+		$mail->isSMTP();
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$mail->SMTPDebug = 2;
+		//Ask for HTML-friendly debug output
+		$mail->Debugoutput = 'html';
+		//Set the hostname of the mail server
+		$mail->Host = 'smtp.gmail.com';
+		// use
+		// $mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$mail->Port = 465;
+		//Set the encryption system to use - ssl (deprecated) or tls
+		$mail->SMTPSecure = 'ssl';
+		//Whether to use SMTP authentication
+		$mail->SMTPAuth = true;
+		//Username to use for SMTP authentication - use full email address for gmail
+		$mail->Username = "admin@nyis.com";
+		//Password to use for SMTP authentication
+		$mail->Password = "flvlzndkpmlhswle";
+		//Set who the message is to be sent from
+		$mail->setFrom('admin@nyis.com', 'NYIS');
+		//Set an alternative reply-to address
+		$mail->addReplyTo('admin@nyis.com', 'Reply');
+		//Set who the message is to be sent to
+		$mail->addAddress('colakang@gmail.com', 'C.K');
+		//Set the subject line
+		$mail->Subject = 'PHPMailer GMail SMTP test';
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+		//Replace the plain text body with one created manually
+		$mail->AltBody = 'This is a plain-text message body';
+		//Attach an image file
+		//$mail->addAttachment('images/phpmailer_mini.png');
+		$mail->Body = "Test Mail";
+		//send the message, check for errors
+		if (!$mail->send()) {
+		    echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+		    echo "Message sent!";
+		}
 		exit();
 	}
 
--- application/index/event/Cases.php
+++ application/index/event/Cases.php
@@ -70,4 +70,58 @@ class Cases
 		else
 			return true;
 	}
+
+	public function sendmail($toAddress="admin@nyis.com",$toName="test",$body="Test Mail",$subject='New Case')
+	{
+		$mail = new \PHPMailer(true);
+		$mail->isSMTP();
+		//Enable SMTP debugging
+		// 0 = off (for production use)
+		// 1 = client messages
+		// 2 = client and server messages
+		$mail->SMTPDebug = 0;
+		//Ask for HTML-friendly debug output
+		$mail->Debugoutput = 'html';
+		//Set the hostname of the mail server
+		$mail->Host = 'smtp.gmail.com';
+		// use
+		// $mail->Host = gethostbyname('smtp.gmail.com');
+		// if your network does not support SMTP over IPv6
+		//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+		$mail->Port = 465;
+		//Set the encryption system to use - ssl (deprecated) or tls
+		$mail->SMTPSecure = 'ssl';
+		//Whether to use SMTP authentication
+		$mail->SMTPAuth = true;
+		//Username to use for SMTP authentication - use full email address for gmail
+		$mail->Username = "admin@nyis.com";
+		//Password to use for SMTP authentication
+		$mail->Password = "flvlzndkpmlhswle";
+		//Set who the message is to be sent from
+		$mail->setFrom('admin@nyis.com', 'NYIS');
+		//Set an alternative reply-to address
+		$mail->addReplyTo('admin@nyis.com', 'Reply');
+		//Set who the message is to be sent to
+		$mail->addAddress($toAddress, $toName);
+		//$mail->addAddress("colakang@gmail.com", $toName);
+		//Set the subject line
+		$mail->Subject = $subject;
+		//Read an HTML message body from an external file, convert referenced images to embedded,
+		//convert HTML into a basic plain-text alternative body
+		//$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+		//Replace the plain text body with one created manually
+		$mail->AltBody = 'This is a plain-text message body';
+		//Attach an image file
+		//$mail->addAttachment('images/phpmailer_mini.png');
+		$mail->Body = $body;
+		//send the message, check for errors
+		if (!$mail->send()) {
+			return false;
+		    //echo "Mailer Error: " . $mail->ErrorInfo;
+		} else {
+			return true;
+		    //echo "Message sent!";
+		}
+	}
+
 }
